899c7714874bd85b787c0e2ea0321e3f73349378
timliu <tim13720757304@163.com>
2021-10-22 15:06:53 +0800

    第一次提交本地仓库

---
+++ TestMoudle/src/com/msb/Student.java
@@ -0,0 +1,15 @@
+package com.msb;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+/**
+ * @Auther: timliu
+ * @Date: 2021/10/22 - 10 - 22 - 15:05
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈");
+    }
+}
